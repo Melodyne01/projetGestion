acca0f445b05dd106148815a38d97ac71cf06a19
TBaldewyns <94040981+tbaldewyns@users.noreply.github.com>
2022-12-19 21:52:57 +0100

    changeDatabase

--- BDProject/settings.py
+++ BDProject/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'BDProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'sql_server.pyodbc',
-        'NAME': 'dev',
+        'NAME': 'prod',
         'USER': 'Groot@helb-ba3-2022-g1',
         'PASSWORD': '6UtHsEI0wL7iKFuJjgQH',
         'HOST': 'helb-ba3-2022-g1.database.windows.net',
