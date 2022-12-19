afa8c0d9d846fd3fd789589c90d08419a1a972d8
TBaldewyns <94040981+tbaldewyns@users.noreply.github.com>
2022-12-19 22:06:16 +0100

    changeDebug

--- BDProject/settings.py
+++ BDProject/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e!frpk-%_9)8y8ckj&c8)01u$a-1lkz83_r#vhprlqcfv^#)&%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
